ef087b55743837ca5b7496a56a919ef866bb74e2
Jun <mr.junlobo@gmail.com>
2020-06-11 14:42:47 +1000

    add https

--- src/app/ipservice.service.ts
+++ src/app/ipservice.service.ts
@@ -9,6 +9,6 @@ export class IPServiceService {
   constructor(private http:HttpClient) { }
   public getIPAddress()
   {
-    return this.http.get("http://api.ipify.org/?format=json");
+    return this.http.get("https://api.ipify.org/?format=json");
   }
 }
